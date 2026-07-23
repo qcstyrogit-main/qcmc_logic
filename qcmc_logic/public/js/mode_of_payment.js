30e0db2b1324f8850885ea2dc9d9e099208d2799
eryk-qcmc <eguilalas@qcstyro.com>
2026-07-23 16:14:01 +0800

    feat: Refine account filtering logic in Mode of Payment setup for improved accuracy

--- qcmc_logic/public/js/mode_of_payment.js
+++ qcmc_logic/public/js/mode_of_payment.js
@@ -2,15 +2,22 @@ frappe.ui.form.on("Mode of Payment", {
 	setup(frm) {
 		frm.set_query("default_account", "accounts", function(doc, cdt, cdn) {
 			const row = locals[cdt][cdn];
-			const account_types = ["Bank", "Cash", "Receivable"];
+			const mode_of_payment = doc.mode_of_payment || "";
 
-			if (doc.mode_of_payment === "Intercompany Collection") {
-				account_types.push("Current Asset");
+			if (/collected by/i.test(mode_of_payment)) {
+				return {
+					filters: [
+						["Account", "account_type", "=", "Current Asset"],
+						["Account", "account_name", "like", "%Advances%"],
+						["Account", "is_group", "=", 0],
+						["Account", "company", "=", row.company],
+					],
+				};
 			}
 
 			return {
 				filters: [
-					["Account", "account_type", "in", account_types],
+					["Account", "account_type", "in", ["Bank", "Cash", "Receivable"]],
 					["Account", "is_group", "=", 0],
 					["Account", "company", "=", row.company],
 				],
