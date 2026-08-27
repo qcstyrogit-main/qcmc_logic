74256e4bc0d189a6ad7616084436fcf7016d9cf0
qcstyrogit-main <qcstyrogit@gmail.com>
2026-08-27 15:57:33 +0800

    feat: Update filters for Inventory Dimension retrieval to handle disabled field dynamically

--- qcmc_logic/patches/add_storage_location_to_pick_list.py
+++ qcmc_logic/patches/add_storage_location_to_pick_list.py
@@ -4,7 +4,12 @@ import frappe
 def execute():
 	from qcmc_logic.overrides.inventory_dimension import CustomInventoryDimension
 
-	for name in frappe.get_all("Inventory Dimension", filters={"disabled": 0}, pluck="name"):
+	filters = (
+		{"disabled": 0}
+		if frappe.get_meta("Inventory Dimension").has_field("disabled")
+		else None
+	)
+	for name in frappe.get_all("Inventory Dimension", filters=filters, pluck="name"):
 		dimension = frappe.get_doc("Inventory Dimension", name)
 		CustomInventoryDimension.add_pick_list_item_fields(dimension)
 
