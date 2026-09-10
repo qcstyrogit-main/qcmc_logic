4b7d3bfd40d74db78f7b30c166b2397320de02bf
qcstyrogit-main <qcstyrogit@gmail.com>
2026-09-10 15:00:24 +0800

    fix: group delivery receipt manufacturing rows

--- qcmc_logic/patches/add_manufacturing_details_to_delivery_receipt_pr.py
+++ qcmc_logic/patches/add_manufacturing_details_to_delivery_receipt_pr.py
@@ -22,6 +22,14 @@ NEW_ITEM_CELLS = """    <td style=\"width: 60px;\">{{ item.qty }}</td>
     <td style=\"width: 75px;\">{{ frappe.utils.formatdate(item.custom_manufacture_date) if item.custom_manufacture_date else '' }}</td>
     <td style=\"width: 75px;\">{{ item.custom_lot_number or '' }}</td>
     <td style=\"width: 50px;\">{{ item.custom_quantity if item.custom_quantity is not none else '' }}</td>"""
+UNCONDITIONAL_DETAIL_ROW = """  <tr>
+    <td colspan=\"4\" style=\"padding-left: 3px;\">
+      {{ frappe.utils.formatdate(item.custom_manufacture_date) if item.custom_manufacture_date else '' }}
+      &nbsp;&nbsp; {{ item.custom_lot_number or '' }}
+      &nbsp;&nbsp; {{ item.custom_quantity if item.custom_quantity is not none else '' }}
+    </td>
+  </tr>
+"""
 
 
 def get_labeled_item_rows(original_item_cells):
@@ -35,15 +43,66 @@ def get_labeled_item_rows(original_item_cells):
     </td>"""
 
 
-def get_target_item_rows(original_item_cells):
+def get_unconditional_item_rows(original_item_cells):
 	return f"""{original_item_cells}
   </tr>
   <tr>
     <td colspan=\"4\" style=\"padding-left: 3px;\">
       {{{{ frappe.utils.formatdate(item.custom_manufacture_date) if item.custom_manufacture_date else '' }}}}
       &nbsp;&nbsp; {{{{ item.custom_lot_number or '' }}}}
-      &nbsp;&nbsp; {{{{ item.custom_quantity if item.custom_quantity is not none else '' }}}}
+	  &nbsp;&nbsp; {{{{ item.custom_quantity if item.custom_quantity is not none else '' }}}}
     </td>"""
+
+
+def get_full_width_conditional_item_rows(original_item_cells):
+	return f"""{original_item_cells}
+  </tr>
+  <tr{{% if not (item.custom_manufacture_date or item.custom_lot_number or item.custom_quantity) %}} style=\"display: none;\"{{% endif %}}>
+    <td colspan=\"4\" style=\"padding-left: 3px;\">
+      {{{{ frappe.utils.formatdate(item.custom_manufacture_date) if item.custom_manufacture_date else '' }}}}
+      &nbsp;&nbsp; {{{{ item.custom_lot_number or '' }}}}
+	  &nbsp;&nbsp; {{{{ item.custom_quantity or '' }}}}
+    </td>"""
+
+
+def get_target_item_rows(original_item_cells):
+	return f"""{original_item_cells}
+  </tr>
+  <tr{{% if not (item.custom_manufacture_date or item.custom_lot_number or item.custom_quantity) %}} style=\"display: none;\"{{% endif %}}>
+    <td></td>
+    <td></td>
+    <td>{{{{ frappe.utils.formatdate(item.custom_manufacture_date) if item.custom_manufacture_date else '' }}}}</td>
+    <td>
+      <span style=\"display: inline-block; width: 65%;\">{{{{ item.custom_lot_number or '' }}}}</span>
+      <span>{{{{ item.custom_quantity or '' }}}}</span>
+    </td>"""
+
+
+def get_grouped_item_rows_without_space(original_item_cells):
+	return f"""{original_item_cells}
+  </tr>
+  {{% endfor %}}
+
+  {{% for item in doc.items %}}
+  <tr{{% if not (item.custom_manufacture_date or item.custom_lot_number or item.custom_quantity) %}} style=\"display: none;\"{{% endif %}}>
+    <td></td>
+    <td></td>
+    <td>{{{{ frappe.utils.formatdate(item.custom_manufacture_date) if item.custom_manufacture_date else '' }}}}</td>
+    <td>
+      <span style=\"display: inline-block; width: 65%;\">{{{{ item.custom_lot_number or '' }}}}</span>
+      <span>{{{{ item.custom_quantity or '' }}}}</span>
+    </td>
+  </tr>
+  {{% endfor %}}"""
+
+
+def get_grouped_item_rows(original_item_cells):
+	grouped_rows = get_grouped_item_rows_without_space(original_item_cells)
+	return grouped_rows.replace(
+		"  {% for item in doc.items %}",
+		"  <tr><td colspan=\"4\">&nbsp;</td></tr>\n\n  {% for item in doc.items %}",
+		1,
+	)
 
 
 def execute():
@@ -53,10 +112,32 @@ def execute():
 			continue
 		normalized_html = html.replace("\r\n", "\n")
 		target_item_rows = get_target_item_rows(old_item_cells)
-		if target_item_rows in normalized_html:
+		grouped_item_rows = get_grouped_item_rows(old_item_cells)
+		if grouped_item_rows in normalized_html:
 			continue
+		grouped_item_rows_without_space = get_grouped_item_rows_without_space(old_item_cells)
+		if grouped_item_rows_without_space in normalized_html:
+			updated_html = normalized_html.replace(
+				grouped_item_rows_without_space,
+				grouped_item_rows,
+				1,
+			)
+		else:
+			updated_html = normalized_html
+		if target_item_rows in normalized_html:
+			updated_html = normalized_html
+		full_width_conditional_item_rows = get_full_width_conditional_item_rows(old_item_cells)
+		unconditional_item_rows = get_unconditional_item_rows(old_item_cells)
 		labeled_item_rows = get_labeled_item_rows(old_item_cells)
-		if labeled_item_rows in normalized_html:
+		if grouped_item_rows_without_space in normalized_html:
+			pass
+		elif target_item_rows in normalized_html:
+			pass
+		elif full_width_conditional_item_rows in normalized_html:
+			updated_html = normalized_html.replace(full_width_conditional_item_rows, target_item_rows, 1)
+		elif unconditional_item_rows in normalized_html:
+			updated_html = normalized_html.replace(unconditional_item_rows, target_item_rows, 1)
+		elif labeled_item_rows in normalized_html:
 			updated_html = normalized_html.replace(labeled_item_rows, target_item_rows, 1)
 		elif NEW_ITEM_CELLS in normalized_html:
 			updated_html = normalized_html.replace(NEW_ITEM_CELLS, target_item_rows, 1)
@@ -67,6 +148,12 @@ def execute():
 				f"The expected item row was not found in {print_format}.",
 				"Delivery Receipt update skipped",
 			)
+			continue
+
+		updated_html = updated_html.replace(UNCONDITIONAL_DETAIL_ROW, "")
+		interleaved_item_rows = target_item_rows + "\n  </tr>\n  {% endfor %}"
+		updated_html = updated_html.replace(interleaved_item_rows, grouped_item_rows, 1)
+		if updated_html == normalized_html:
 			continue
 
 		frappe.db.set_value(
